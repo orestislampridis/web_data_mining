7c9549d564a16e508f96bfbba48f532729b2cf42
Chrysovalantis Kontoulis <38863812+valantiskon@users.noreply.github.com>
2020-05-13 00:03:43 +0300

    Added features
    Added contractions, lemmatization, cleaned some code

--- task 2/clean_text.py
+++ task 2/clean_text.py
@@ -1,11 +1,74 @@
+# pre-process and clean data
+
 import string
 import re
 import string
 
+from nltk.stem import *  # from nltk.stem.porter import *
+from nltk.stem.snowball import SnowballStemmer
+from nltk import wordnet  # used for lemmatizer
+
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
-stop_words = set(stopwords.words('english'))
+
+
+# ======================================================================================================================
+# Remove Contractions (pre-processing)
+# ======================================================================================================================
+
+def get_contractions():
+    contraction_dict = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
+                        "could've": "could have", "couldn't": "could not", "didn't": "did not",
+                        "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not",
+                        "haven't": "have not", "he'd": "he would", "he'll": "he will", "he's": "he is",
+                        "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
+                        "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have",
+                        "I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have",
+                        "i'll": "i will", "i'll've": "i will have", "i'm": "i am", "i've": "i have",
+                        "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will",
+                        "it'll've": "it will have", "it's": "it is", "let's": "let us", "ma'am": "madam",
+                        "mayn't": "may not", "might've": "might have", "mightn't": "might not",
+                        "mightn't've": "might not have", "must've": "must have", "mustn't": "must not",
+                        "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have",
+                        "o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
+                        "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
+                        "she'd": "she would", "she'd've": "she would have", "she'll": "she will",
+                        "she'll've": "she will have", "she's": "she is", "should've": "should have",
+                        "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have",
+                        "so's": "so as", "this's": "this is", "that'd": "that would",
+                        "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+                        "there'd've": "there would have", "there's": "there is", "here's": "here is",
+                        "they'd": "they would", "they'd've": "they would have", "they'll": "they will",
+                        "they'll've": "they will have", "they're": "they are", "they've": "they have",
+                        "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have",
+                        "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have",
+                        "weren't": "were not", "what'll": "what will", "what'll've": "what will have",
+                        "what're": "what are", "what's": "what is", "what've": "what have", "when's": "when is",
+                        "when've": "when have", "where'd": "where did", "where's": "where is",
+                        "where've": "where have", "who'll": "who will", "who'll've": "who will have",
+                        "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have",
+                        "will've": "will have", "won't": "will not", "won't've": "will not have",
+                        "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have",
+                        "y'all": "you all", "y'all'd": "you all would", "y'all'd've": "you all would have",
+                        "y'all're": "you all are", "y'all've": "you all have", "you'd": "you would",
+                        "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+                        "you're": "you are", "you've": "you have"}
+
+    contraction_re = re.compile('(%s)' % '|'.join(contraction_dict.keys()))
+    return contraction_dict, contraction_re
+
+def replace_contractions(text):
+    contractions, contractions_re = get_contractions()
+
+    def replace(match):
+        return contractions[match.group(0)]
+
+    return contractions_re.sub(replace, text)
+
+
+whitelist = ["n't", "not", 'nor', "nt"]  # Keep the words "n't" and "not", 'nor' and "nt"
+stop_words = set(list(stopwords.words('english')) + ['"', '|'])
 stop_words.update(['zero','one','two','three','four','five','six','seven','eight','nine','ten','may','also','across','among','beside','however','yet','within'])
 
 
@@ -32,6 +95,7 @@ emoji_pattern = re.compile("["
 emoticons = emoticons_happy.union(emoticons_sad)
 
 
+
 def strip_links(text):
     link_regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
     links = re.findall(link_regex, text)
@@ -55,11 +119,15 @@ def strip_all_entities(text):
 
 #fumction for stemming
 def stemming(text):
-    stemmer = PorterStemmer()
+    # stemmer = PorterStemmer()
+    # stemmer = SnowballStemmer("english")
+    lemmatizer = WordNetLemmatizer()
     stemmed_text = ""
     for word in text.split():
-        stem = stemmer.stem(word)
-        stemmed_text += stem
+        # stem = stemmer.stem(word)
+        # stemmed_text += stem
+        lemma = lemmatizer.lemmatize(word)
+        stemmed_text += lemma
         stemmed_text += " "
     stemmed_text = stemmed_text.strip()
     return stemmed_text
@@ -80,14 +148,15 @@ def clean_tweets(tweet):
     tweet = strip_all_entities(strip_links(tweet))
 
     # remove rt and via in case of tweet data
-    tweet = tweet.lower().split()
-    tweet = [w for w in tweet]
-    tweet = " ".join(tweet)
+    tweet = tweet.lower()
     tweet = re.sub(r"rt", "", tweet)
     tweet = re.sub(r"via", "", tweet)
 
     # replace consecutive non-ASCII characters with a space
     tweet = re.sub(r'[^\x00-\x7F]+',' ', tweet)
+
+    # substitute contractions with full words
+    tweet = replace_contractions(tweet)
 
     # remove emojis from tweet
     tweet = emoji_pattern.sub(r'', tweet)
@@ -97,7 +166,7 @@ def clean_tweets(tweet):
     # looping through conditions
     for w in word_tokens:
         # check tokens against stop words, emoticons and punctuations
-        if w not in stop_words and w not in emoticons and w not in string.punctuation:
+        if (w not in stop_words and w not in emoticons and w not in string.punctuation) or w in whitelist:
             filtered_tweet.append(w)
     return ' '.join(filtered_tweet)
 
