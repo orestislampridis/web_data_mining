6a5ee201dd245bd67010aa4a8ae9018dbcc1d459
Chrysovalantis Kontoulis <38863812+valantiskon@users.noreply.github.com>
2020-05-18 01:32:16 +0300

    Clear non-english post and data formating
    Clean non-english posts and additional data formating (typecasting to all data columns)

--- instagram/data_formating.py
+++ instagram/data_formating.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
 from datetime import datetime
+from langdetect import detect_langs
+from langdetect.lang_detect_exception import LangDetectException
+
 
 # reading the JSON data using json.load()
 file = 'dataset/test.json'
@@ -29,23 +32,83 @@ for x in tweets:
                 elif key == "$oid":
                     x[column] = str(val)
 
+# ======================================================================================================================
+
+# column list containing columns that needed data type conversion
 column_list = set(column_list)
-print(column_list)
+print("column_list for columns that need cleaning from nested jsons: ", column_list)
 print(len(column_list))
 
+# ======================================================================================================================
+
+# Bson (mongoDB's data types on nested jsons)
 keys_list = set(keys_list)
-print(keys_list)
+print("Bson keys_list to remove: ", keys_list)
 print(len(keys_list))
 
-test = pd.DataFrame(tweets)
-print(test)
+# ======================================================================================================================
 
-clist = list(test.columns)  # list of all column names
+# convert data structure to dataframe
+df = pd.DataFrame(tweets)
+
+# ======================================================================================================================
+
+# columns that still need data conversion
+rest_columns = list(df.columns)
+for column in column_list:
+    rest_columns.remove(column)
+print("columns that still need data conversion: ", rest_columns)
+print(len(rest_columns))
+
+# ======================================================================================================================
+
+# Data conversion to the rest columns (that still need data conversion)
+df["caption"] = df["caption"].astype(str)
+df["caption_mentions"] = df["caption_mentions"].astype(str)
+df["caption_hashtags"] = df["caption_hashtags"].astype(str)
+df["tagged_users"] = df["tagged_users"].astype(str)
+#print(df["location"])
+df["location"] = df["location"].astype(str)  # MIGHT BE COORDINATES
+#print(df["_location"])
+df["_location"] = df["_location"].astype(str)  # MIGHT BE COORDINATES
+df["is_video"] = df["is_video"].astype(bool)
+df["owner_private"] = df["owner_private"].astype(bool)
+df["owner_viewable_story"] = df["owner_viewable_story"].astype(bool)
+df["owner_verified"] = df["owner_verified"].astype(bool)
+df["owner_username"] = df["owner_username"].astype(str)
+
+# ======================================================================================================================
+
+# filter all the non-english posts (with probability of being english less than 0.6)
+print(df.shape)
+for index, row in df['caption'].iteritems():
+    try:
+        languages = detect_langs(row)  # detect_langs: predict the languanges in text and their probabilities
+
+        langs = [l.lang for l in languages]  # get a list with all languages
+        langs_probas = [l.prob for l in languages]  # get a list with all language probabilities
+        #print("text lang stats: ", langs, langs_probas, langs.index('en'))
+
+        if langs_probas[langs.index('en')] < 0.6:  # if the probability of the text language being english is less than 0.5
+            df = df[df.caption != row]  # drop the row
+    except LangDetectException:  # in case detect finds sentence that do not contain strings. i.e. only numbers
+        df = df[df.caption != row]  # drop the row
+    except ValueError:  # in case the post in not in english (catch exception of langs.index('en'))
+        df = df[df.caption != row]  # drop the row
+
+print(df)
+print(df.shape)
+
+# ======================================================================================================================
+
+clist = list(df.columns)  # list of all column names
 print(clist)
 print(len(clist))  # number of all columns
 
+# ======================================================================================================================
+
 # save cleaned data, from dataframe, to csv
-test.to_csv('dataset/test_cleaned.csv', index=False, encoding='utf-8')
+df.to_csv('dataset/test_cleaned.csv', index=False, encoding='utf-8')
 
 data = pd.read_csv("dataset/test_cleaned.csv", index_col=False)
 
