d3100515ea21a901e734982328c0e79fc50afaf2
ptzallas <ptzallas@csd.auth.gr>
2020-05-27 19:58:02 +0300

    emotion vader textblob instagram and twitter

--- task_3/Sentiment_Vader_TextBlob.py
+++ task_3/Sentiment_Vader_TextBlob.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
-import numpy as np
+
 from twitter.connect_mongo import read_mongo
 
 
@@ -13,14 +13,18 @@ file1="../dataset/test_cleaned.csv"
 insta=pd.read_csv(file1, encoding="utf8")
 
 tweets = read_mongo(db='twitter_db', collection='twitter_collection', query={'text': 1})
-tweets = tweets.sample(n=100, random_state=42)
+tweets = tweets.sample(n=135, random_state=42)
+
+# print(tweets)
 
 insta = insta[['_id', 'caption' ]] #this is all I need
+#tweets= tweets[['_id','text']]
 
 vader = SentimentIntensityAnalyzer()
 
+#check Vader and TextBlob sentiment score for INSTAGRAM POSTS
 vader_score = []  # empty list to store'compound' VADER scores
-textblob_score = []
+textblob_score = [] # empty list to store 'polarity' TextBlob scores
 for i in range(0, len(insta)):
     k = vader.polarity_scores(insta.iloc[i]['caption'])
     l = TextBlob(insta.iloc[i]['caption']).sentiment
@@ -28,45 +32,133 @@ for i in range(0, len(insta)):
     vader_score.append(k)
     textblob_score.append(l)
 
-vader_score=pd.DataFrame.from_dict(vader_score)
-textblob_score=pd.DataFrame.from_dict(textblob_score)
+vader_score_insta=pd.DataFrame.from_dict(vader_score)
+textblob_score_insta=pd.DataFrame.from_dict(textblob_score)
 
-insta['VADER compound score'] = vader_score['compound']
-insta['TextBlob polarity score']=textblob_score['polarity']
+insta['VADER compound score'] = vader_score_insta['compound']
+insta['TextBlob polarity score']=textblob_score_insta['polarity']
+
+
+
+
+#check Vader and TextBlob sentiment score for TWEETS
+vader_score = []  # empty list to store'compound' VADER scores
+textblob_score = [] # empty list to store 'polarity' TextBlob scores
+for i in range(0, len(tweets)):
+    k = vader.polarity_scores(tweets.iloc[i]['text'])
+    l = TextBlob(tweets.iloc[i]['text']).sentiment
+
+    vader_score.append(k)
+    textblob_score.append(l)
+
+vader_score_tweets=pd.DataFrame.from_dict(vader_score)
+textblob_score_tweets=pd.DataFrame.from_dict(textblob_score)
+
+tweets['VADER compound score'] = vader_score_tweets['compound']
+tweets['TextBlob polarity score']=textblob_score_tweets['polarity']
+
 
 # http://comp.social.gatech.edu/papers/icwsm14.vader.hutto.pdf
 
-pred_V = []
-pred_B = []
+#Predict sentiment in INSTAGRAM POSTS with Vader and TextBlob
+pred_V_insta = []
+pred_B_insta = []
 
 for i in range(0, len(insta)):
     if ((insta.iloc[i]['VADER compound score'] >= 0.4)):
-        pred_V.append('positive')
+        pred_V_insta.append('positive')
 
     elif ((insta.iloc[i]['VADER compound score'] > -0.2) & (insta.iloc[i]['VADER compound score'] < 0.4)):
-        pred_V.append('neutral')
+        pred_V_insta.append('neutral')
 
     elif ((insta.iloc[i]['VADER compound score'] <= -0.2)):
-        pred_V.append('negative')
+        pred_V_insta.append('negative')
 
     if ((insta.iloc[i]['TextBlob polarity score'] >= 0.1)):
-        pred_B.append('positive')
+        pred_B_insta.append('positive')
 
     elif ((insta.iloc[i]['TextBlob polarity score'] > -0.1) & (insta.iloc[i]['TextBlob polarity score'] < 0.1)):
-        pred_B.append('neutral')
+        pred_B_insta.append('neutral')
 
     elif ((insta.iloc[i]['TextBlob polarity score'] <= -0.1)):
-        pred_B.append('negative')
+        pred_B_insta.append('negative')
 
-insta['VADER predicted sentiment'] = pred_V
-insta['TextBlob predicted sentiment'] = pred_B
+insta['VADER predicted sentiment'] = pred_V_insta
+insta['TextBlob predicted sentiment'] = pred_B_insta
+
+
+
+
+#Predict sentiment in TWEETS with Vader and TextBlob
+pred_V_tweets = []
+pred_B_tweets = []
+
+for i in range(0, len(tweets)):
+    if ((tweets.iloc[i]['VADER compound score'] >= 0.4)):
+        pred_V_tweets.append('positive')
+
+    elif ((tweets.iloc[i]['VADER compound score'] > -0.2) & (tweets.iloc[i]['VADER compound score'] < 0.4)):
+        pred_V_tweets.append('neutral')
+
+    elif ((tweets.iloc[i]['VADER compound score'] <= -0.2)):
+        pred_V_tweets.append('negative')
+
+    if ((tweets.iloc[i]['TextBlob polarity score'] >= 0.1)):
+        pred_B_tweets.append('positive')
+
+    elif ((tweets.iloc[i]['TextBlob polarity score'] > -0.1) & (tweets.iloc[i]['TextBlob polarity score'] < 0.1)):
+        pred_B_tweets.append('neutral')
+
+    elif ((tweets.iloc[i]['TextBlob polarity score'] <= -0.1)):
+        pred_B_tweets.append('negative')
+
+tweets['VADER predicted sentiment'] = pred_V_tweets
+tweets['TextBlob predicted sentiment'] = pred_B_tweets
+
+
+
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 400)
+print("Instagram sentiment predictions")
 print(insta.head())
+print("\nTwitter sentiment predictions")
+print(tweets.head())
 
-#γρρηγορο πλοταριμα
-insta.groupby('VADER predicted sentiment').size().plot(kind='bar')
+
+#Pie plots for INSTAGRAM
+fig = plt.figure(figsize=(10,10))
+ax_insta_V = fig.add_axes([0,0,1,1])
+ax_insta_V.axis('equal')
+colors = ['#ff9999','#66b3ff','#99ff99']
+ax_insta_V.pie(insta.groupby('VADER predicted sentiment').size(),colors=colors, autopct='%1.1f%%',labels=insta.groupby('VADER predicted sentiment').size().index, shadow=True, startangle=90,textprops={'fontsize': 14})
+plt.title('Instagram/n VADER predicted sentiment', bbox={'facecolor':'0.8', 'pad':5})
 plt.show()
-insta.groupby('TextBlob predicted sentiment').size().plot(kind='bar')
+
+fig = plt.figure(figsize=(10,10))
+
+ax_insta_B = fig.add_axes([0,0,1,1])
+ax_insta_B.axis('equal')
+colors = ['#ff9999','#66b3ff','#99ff99']
+ax_insta_B.pie(insta.groupby('TextBlob predicted sentiment').size(),colors=colors, autopct='%1.1f%%',labels=insta.groupby('VADER predicted sentiment').size().index, shadow=True, startangle=90,textprops={'fontsize': 14})
+plt.title('Instagram/n TextBlob predicted sentiment', bbox={'facecolor':'0.8', 'pad':5})
+plt.show()
+
+
+#Pie plots for TWEETER
+fig = plt.figure(figsize=(10,10))
+ax_insta_V = fig.add_axes([0,0,1,1])
+ax_insta_V.axis('equal')
+colors = ['#ff9999','#66b3ff','#99ff99']
+ax_insta_V.pie(tweets.groupby('VADER predicted sentiment').size(),colors=colors, autopct='%1.1f%%',labels=tweets.groupby('VADER predicted sentiment').size().index, shadow=True, startangle=90,textprops={'fontsize': 14})
+plt.title('Instagram/n VADER predicted sentiment', bbox={'facecolor':'0.8', 'pad':5})
+plt.show()
+
+fig = plt.figure(figsize=(10,10))
+
+ax_insta_B = fig.add_axes([0,0,1,1])
+ax_insta_B.axis('equal')
+colors = ['#ff9999','#66b3ff','#99ff99']
+ax_insta_B.pie(tweets.groupby('TextBlob predicted sentiment').size(),colors=colors, autopct='%1.1f%%',labels=tweets.groupby('VADER predicted sentiment').size().index, shadow=True, startangle=90,textprops={'fontsize': 14})
+plt.title('Instagram/n TextBlob predicted sentiment', bbox={'facecolor':'0.8', 'pad':5})
 plt.show()
