f5578cafe891cca0f492802d527e50c045a141ce
Chrysovalantis Kontoulis <38863812+valantiskon@users.noreply.github.com>
2020-06-16 15:18:48 +0300

    init

---
+++ task_7/request.py
@@ -0,0 +1,8 @@
+import requests
+
+# requests module to call APIs already defined in app.py and displays the returned value
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+
+print(r.json())
