dee1450ba0afb550405941ccabdea9395aac290e
Chrysovalantis Kontoulis <38863812+valantiskon@users.noreply.github.com>
2020-06-21 00:46:57 +0300

    Add files via upload

--- task_3/predict_affective.py
+++ task_3/predict_affective.py
@@ -1,28 +1,27 @@
 import pickle
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
-from tqdm import tqdm
-
+import plotly.offline as py
+import plotly.graph_objects as go
 import task_2.preprocessing
-
+from tqdm import tqdm
 tqdm.pandas()
 from connect_mongo import read_mongo
 
-# get categories
-emotion_categories = ['anger', 'joy', 'disgust', 'fear', 'sadness', 'surprise']
-# read data
-file1 = "../dataset/insta_data_cleaned.csv"
-insta = pd.read_csv(file1, sep='~', encoding="utf8")
 
-tweets = read_mongo(db='twitter_db', collection='twitter_collection', query={'text': 1})
-tweets = tweets.sample(n=1000, random_state=42)
+#get categories
+emotion_categories = ['anger', 'joy', 'disgust', 'fear', 'sadness', 'surprise']
+#read data
+insta = pd.read_csv("../dataset/insta_data_cleaned.csv", sep='~', index_col=False)
+
+tweets = read_mongo(db='twitter_db', collection='twitter_collection', query={'text': 1, 'created_at': 1})
+
 
 reading = task_2.preprocessing.preprocessing(convert_lower=False, use_spell_corrector=True)
 insta['caption'] = insta.caption.progress_map(reading.clean_text)
-tweets['text'] = tweets.text.progress_map(reading.clean_text)
+tweets['text']=tweets.text.progress_map(reading.clean_text)
 
 #print(SemEval['Tweet'].iloc[:2])
 def list2string(list):
@@ -61,6 +60,101 @@ result_twitter = pd.concat([tweets,pred_twitter], axis=1, sort=False)
 
 
 
+print("result_insta", result_insta)
+print(result_insta.columns.to_list())
+print("result_twitter", result_twitter)
+print(result_twitter.columns.to_list())
+print(emotion_categories)
+
+
+# ======================================================================================================================
+# Create time series
+# ======================================================================================================================
+
+# USED FOR PLOTTING
+result_insta.rename(columns={'date': 'created_at'}, inplace=True)  # rename column date to created_at
+result_insta = result_insta.sort_values(by=['created_at'])
+
+result_insta['datetime'] = pd.to_datetime(result_insta['created_at'])
+result_insta = result_insta.set_index('datetime')
+result_insta.drop(['created_at'], axis=1, inplace=True)
+print(result_insta.columns)
+
+
+# ======================================================================================================================
+# Plot in interactive graph the number of extracted post per time period
+# ======================================================================================================================
+
+print("data.index.unique() ", result_insta.index.map(lambda t: t.date()).unique())
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['anger'],
+                    mode='lines+markers',
+                    name='anger'))
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['joy'],
+                    mode='lines+markers',
+                    name='joy'))
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['disgust'],
+                    mode='lines+markers',
+                    name='disgust'))
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['fear'],
+                    mode='lines+markers',
+                    name='fear'))
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['sadness'],
+                    mode='lines+markers',
+                    name='sadness'))
+fig.add_trace(go.Scatter(x=result_insta.index.map(lambda t: t.date()).unique(), y=result_insta['surprise'],
+                    mode='lines+markers',
+                    name='surprise'))
+
+py.plot(fig, filename='insta_emotion_per_day.html')
+
+
+# ======================================================================================================================
+# Create time series
+# ======================================================================================================================
+
+# USED FOR PLOTTING
+result_twitter = result_twitter.sort_values(by=['created_at'])
+
+result_twitter['datetime'] = pd.to_datetime(result_twitter['created_at'])
+result_twitter = result_twitter.set_index('datetime')
+result_twitter.drop(['created_at'], axis=1, inplace=True)
+print(result_twitter.columns)
+
+
+# ======================================================================================================================
+# Plot in interactive graph the number of extracted post per time period
+# ======================================================================================================================
+
+print("data.index.unique() ", result_twitter.index.map(lambda t: t.date()).unique())
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['anger'],
+                    mode='lines+markers',
+                    name='anger'))
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['joy'],
+                    mode='lines+markers',
+                    name='joy'))
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['disgust'],
+                    mode='lines+markers',
+                    name='disgust'))
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['fear'],
+                    mode='lines+markers',
+                    name='fear'))
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['sadness'],
+                    mode='lines+markers',
+                    name='sadness'))
+fig.add_trace(go.Scatter(x=result_twitter.index.map(lambda t: t.date()).unique(), y=result_twitter['surprise'],
+                    mode='lines+markers',
+                    name='surprise'))
+
+py.plot(fig, filename='twitter_emotion_per_day.html')
+
+
+# ======================================================================================================================
 
 
 #plot results Instagram
