b8845402a9cccc20fde0e0e9e5dc77431e4fa2cd
Chrysovalantis Kontoulis <38863812+valantiskon@users.noreply.github.com>
2020-06-20 04:39:27 +0300

    Add files via upload

--- task_7/app.py
+++ task_7/app.py
@@ -20,6 +20,10 @@ def home():
     return render_template('index.html', subtask_title='This is the Main Page. Welcome')
 
 
+# ======================================================================================================================
+# TASK 3
+# ======================================================================================================================
+
 @app.route('/ner', methods=['POST'])
 def ner():
 
@@ -46,23 +50,18 @@ def ner():
 @app.route('/collocations', methods=['POST'])
 def collocations():
 
-    path = os.getcwd() + "\\static\\results\\named entities"
+    path = os.getcwd() + "\\static\\results\\Collocations"
 
     # ======================================================================================================================
 
     subtitle_images = []
     for image_name in os.listdir(path):
         # create the full input path and read the file
-        input_path = os.path.join("\\static\\results\\named entities", image_name)
+        input_path = os.path.join("\\static\\results\\Collocations", image_name)
         subtitle_images.append((image_name.replace('.png', ''), input_path))
     print(subtitle_images)
 
-    # subtitle_images = [('This is the distribution of gender', '\\static\\apps.png')]
-    #print(os.getcwd() + '\\static\\apps.png')
-
-    subtitle_html = [('HTML HERE', '\\templates\\gender-distribution.html')]
-
-    return render_template('index.html', subtask_title='Named Entity Recognition', static_plots=subtitle_images, dynamic_plots=subtitle_html)
+    return render_template('index.html', subtask_title='Collocations', static_plots=subtitle_images)
 
 
 
@@ -130,38 +129,32 @@ def dtm():
 @app.route('/affect_analysis', methods=['POST'])
 def affect_analysis():
 
-    path = os.getcwd() + "\\static\\results\\named entities"
+    path = os.getcwd() + "\\static\\results\\affective analysis"
 
     # ======================================================================================================================
 
     subtitle_images = []
     for image_name in os.listdir(path):
         # create the full input path and read the file
-        input_path = os.path.join("\\static\\results\\named entities", image_name)
+        input_path = os.path.join("\\static\\results\\affective analysis", image_name)
         subtitle_images.append((image_name.replace('.png', ''), input_path))
     print(subtitle_images)
 
-    # subtitle_images = [('This is the distribution of gender', '\\static\\apps.png')]
-    #print(os.getcwd() + '\\static\\apps.png')
-
-    subtitle_html = [('HTML HERE', '\\templates\\gender-distribution.html')]
-
-    return render_template('index.html', subtask_title='Affective Analysis', static_plots=subtitle_images, dynamic_plots=subtitle_html)
+    return render_template('index.html', subtask_title='Affective Analysis', static_plots=subtitle_images)
 
 
 
-# EXAMPLE 2
 @app.route('/sentim_analysis', methods=['POST'])
 def sentim_analysis():
 
-    path = os.getcwd() + "\\static\\results\\sentiment\\imgs"
+    path = os.getcwd() + "\\static\\results\\sentiment detection"
 
     # ======================================================================================================================
 
     subtitle_images = []
     for image_name in os.listdir(path):
         # create the full input path and read the file
-        input_path = os.path.join("\\static\\results\\sentiment\\imgs", image_name)
+        input_path = os.path.join("\\static\\results\\sentiment detection", image_name)
         subtitle_images.append((image_name.replace('.png', ''), input_path))
     print(subtitle_images)
 
@@ -172,6 +165,164 @@ def sentim_analysis():
 
     return render_template('index.html', subtask_title='Sentiment Analysis', static_plots=subtitle_images, dynamic_plots=subtitle_html)
 
+
+# ======================================================================================================================
+# TASK 5
+# ======================================================================================================================
+
+@app.route('/age_detect', methods=['POST'])
+def age_detect():
+
+    path = os.getcwd() + "\\static\\results\\age prediction"
+
+    # ======================================================================================================================
+
+    subtitle_images = []
+    for image_name in os.listdir(path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\age prediction", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+    print(subtitle_images)
+
+    return render_template('index.html', subtask_title='Age Detection', static_plots=subtitle_images)
+
+
+
+@app.route('/gend_detect', methods=['POST'])
+def gend_detect():
+
+    path = os.getcwd() + "\\static\\results\\gender detection\\imgs"
+
+    # ======================================================================================================================
+
+    subtitle_images = []
+    for image_name in os.listdir(path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\gender detection\\imgs", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+    print(subtitle_images)
+
+
+    subtitle_html = [('HTML HERE', '\\templates\\gender-distribution.html')]
+
+    return render_template('index.html', subtask_title='Sentiment Analysis', static_plots=subtitle_images, dynamic_plots=subtitle_html)
+
+
+@app.route('/person_detect', methods=['POST'])
+def person_detect():
+
+    path = os.getcwd() + "\\static\\results\\Personality Detection"
+
+    # ======================================================================================================================
+
+    subtitle_images = []
+    for image_name in os.listdir(path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\Personality Detection", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+    print(subtitle_images)
+
+    return render_template('index.html', subtask_title='Sentiment Analysis', static_plots=subtitle_images)
+
+
+# ======================================================================================================================
+# TASK 6
+# ======================================================================================================================
+
+@app.route('/base_like', methods=['POST'])
+def base_like():
+
+    twitter_path = os.getcwd() + "\\static\\results\\emerging topics\\twitter\\imgs\\all_topics"
+    twitter_path2 = os.getcwd() + "\\static\\results\\emerging topics\\twitter\\imgs\\emerg_topics"
+
+    insta_path = os.getcwd() + "\\static\\results\\emerging topics\\insta\\imgs\\all_topics"
+    insta_path2 = os.getcwd() + "\\static\\results\\emerging topics\\insta\\imgs\\emerg_topics"
+
+    # ======================================================================================================================
+
+    subtitle_images = []
+
+    # ======================================================================================================================
+
+    for image_name in os.listdir(twitter_path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\emerging topics\\twitter\\imgs\\all_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+
+    for image_name in os.listdir(twitter_path2):
+        input_path2 = os.path.join("\\static\\results\\emerging topics\\twitter\\imgs\\emerg_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path2))
+
+    # ======================================================================================================================
+
+    for image_name in os.listdir(insta_path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\emerging topics\\insta\\imgs\\all_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+
+    for image_name in os.listdir(insta_path2):
+        input_path2 = os.path.join("\\static\\results\\emerging topics\\insta\\imgs\\emerg_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path2))
+
+    # ======================================================================================================================
+
+    print(subtitle_images)
+
+    # subtitle_images = [('This is the distribution of gender', '\\static\\apps.png')]
+    #print(os.getcwd() + '\\static\\apps.png')
+
+    subtitle_html = [('HTML HERE', '\\templates\\gender-distribution.html')]
+
+    return render_template('index.html', subtask_title='Emerging Topics', static_plots=subtitle_images, dynamic_plots=subtitle_html)
+
+
+
+
+@app.route('/nlp_like', methods=['POST'])
+def nlp_like():
+
+    twitter_path = os.getcwd() + "\\static\\results\\emerging topics\\twitter\\imgs\\all_topics"
+    twitter_path2 = os.getcwd() + "\\static\\results\\emerging topics\\twitter\\imgs\\emerg_topics"
+
+    insta_path = os.getcwd() + "\\static\\results\\emerging topics\\insta\\imgs\\all_topics"
+    insta_path2 = os.getcwd() + "\\static\\results\\emerging topics\\insta\\imgs\\emerg_topics"
+
+    # ======================================================================================================================
+
+    subtitle_images = []
+
+    # ======================================================================================================================
+
+    for image_name in os.listdir(twitter_path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\emerging topics\\twitter\\imgs\\all_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+
+    for image_name in os.listdir(twitter_path2):
+        input_path2 = os.path.join("\\static\\results\\emerging topics\\twitter\\imgs\\emerg_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path2))
+
+    # ======================================================================================================================
+
+    for image_name in os.listdir(insta_path):
+        # create the full input path and read the file
+        input_path = os.path.join("\\static\\results\\emerging topics\\insta\\imgs\\all_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path))
+
+    for image_name in os.listdir(insta_path2):
+        input_path2 = os.path.join("\\static\\results\\emerging topics\\insta\\imgs\\emerg_topics", image_name)
+        subtitle_images.append((image_name.replace('.png', ''), input_path2))
+
+    # ======================================================================================================================
+
+    print(subtitle_images)
+
+    # subtitle_images = [('This is the distribution of gender', '\\static\\apps.png')]
+    #print(os.getcwd() + '\\static\\apps.png')
+
+    subtitle_html = [('HTML HERE', '\\templates\\gender-distribution.html')]
+
+    return render_template('index.html', subtask_title='Emerging Topics', static_plots=subtitle_images, dynamic_plots=subtitle_html)
 
 
 if __name__ == "__main__":
